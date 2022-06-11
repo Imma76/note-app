488b4fca0b50de4c7728ad5ac0925c988c006040
Imma76 <emmanuelugwueze6@gmail.com>
2022-06-11 22:17:00 +0100

    connected models to services

--- services/note.services.js
+++ services/note.services.js
@@ -1,17 +1,18 @@
-const getNotes = () => {
+import notes from '../models/note.model.js';
 
-};
+const getNotes = () => notes.find({});
 
 const updateNote = (id) => {
-
+    const update = notes.updateOne({})
 };
 
 const createNote = (note) => {
-
+  const create = notes.create(note);
+  return create;
 };
 
 const deleteNote = (id) => {
-
+ const deletenote = notes.deleteOne({ _id: id });
 };
 
 export default getNotes;
