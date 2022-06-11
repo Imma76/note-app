9ff87f7ce431489c8ac3971cc6c4e82bd880f459
Imma76 <emmanuelugwueze6@gmail.com>
2022-06-11 21:38:02 +0100

    initiliazed note services

---
+++ services/note.services.js
@@ -0,0 +1,18 @@
+
+const getNotes = () => {
+    
+}
+
+const updateNote = (id) => {
+    
+}
+
+const createNote = (note) => {
+    
+}
+
+const deleteNote = (id) => {
+    
+}
+
+export default getNotes
