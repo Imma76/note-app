ad34372ef27a9735eb327f2ee5eb2800631357fb
Imma76 <emmanuelugwueze6@gmail.com>
2022-06-11 21:34:49 +0100

    initiliazed note services

---
+++ controllers/note.controller.js
@@ -0,0 +1,18 @@
+class NoteController{
+    async getAllNotes() {
+        
+
+    }
+    async postNewNote() {
+        
+    }
+
+    async updateNote() {
+        
+    }
+    async deleteNote() {
+        
+    }
+}
+
+export default new NoteController()
