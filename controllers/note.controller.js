cf3a97d515c4467c171995d29f2a27fcbfce4f46
Imma76 <emmanuelugwueze6@gmail.com>
2022-06-12 02:59:26 +0100

    implemented check categories

--- controllers/note.controller.js
+++ controllers/note.controller.js
@@ -1,14 +1,19 @@
 /* eslint-disable class-methods-use-this */
 import noteService from '../services/note.services.js';
+import noteCategory from '../utils/utils.js';
 
 class NoteController {
   async getAllNotes(req, res) {
+   
       const allNotes = await noteService.getNotes();
     return res.status(200).send({ message: true, body: allNotes });
   }
 
   async postNewNote(req, res) {
-    const data = { title: req.body.title, content: req.body.content };
+    if (!noteCategory.includes(req.body.category)) {
+      return res.status(400).send({ message: false, body: `category must be among ${noteCategory} ` });
+    }
+    const data = { title: req.body.title, content: req.body.content , category:req.body.category};
     await noteService.createNote(data);
     return res.status(201).send({ message: true, body: 'article posted successfully' });
   }
