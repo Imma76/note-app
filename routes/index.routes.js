51e9e5474173b7809810f445ca11e6e9b807aa7a
Emmanuel <emmanuelugwueze6@gmail.com>
2022-06-22 21:25:52 +0100

    user index routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import noteRouter from './notes.routes.js';
+import userRouter from './user.route.js';
 
 const router = express.Router();
 
-router.use('/notes', noteRouter);
+router.use('/note-app/notes', noteRouter);
+router.use('/note-app/user', userRouter);
 
 export default router;
