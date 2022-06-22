27f6c1a4336de7ec972b9af8b75994d5af2e22da
Emmanuel <emmanuelugwueze6@gmail.com>
2022-06-22 21:27:24 +0100

    added validators to users routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import userController from '../controllers/user.controller.js';
+import validateUserSchema from '../validators/user.validator.js';
+import validator from '../validators/validator.js';
 
 const userRouter = express.Router();
 
-userRouter.post('/user', userController.createUser);
-userRouter.post('/user', userController.loginUser);
+userRouter.post('/user', [validator(validateUserSchema)], userController.createUser);
+userRouter.post('/user', [validator(validateUserSchema)], userController.loginUser);
 
 export default userRouter;
