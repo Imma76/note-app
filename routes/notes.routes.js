f0ee0692d39b2dc37d1756a5337a6e741c4b91d5
Emmanuel <emmanuelugwueze6@gmail.com>
2022-06-22 20:56:55 +0100

    updated note routes

--- routes/notes.routes.js
+++ routes/notes.routes.js
@@ -5,10 +5,10 @@ import noteValidator from '../validators/note.validator.js';
 
 const noteRouter = express.Router();
 
-noteRouter.get('/all_notes', noteController.getAllNotes);
+noteRouter.get('/notes', noteController.getAllNotes);
 
-noteRouter.post('/new_note', [validator(noteValidator.validateNewNoteSchema)],  noteController.postNewNote);
+noteRouter.post('/notes', [validator(noteValidator.validateNewNoteSchema)],  noteController.postNewNote);
 
-noteRouter.put('/update_note', [validator(noteValidator.updateNoteSchema)], noteController.updateNote);
-noteRouter.delete('/delete_note',[validator(noteValidator.deleteNoteSchema)],  noteController.deleteNote);
+noteRouter.put('/notes', [validator(noteValidator.updateNoteSchema)], noteController.updateNote);
+noteRouter.delete('/notes',[validator(noteValidator.deleteNoteSchema)],  noteController.deleteNote);
 export default noteRouter;
