bec21767faab9f062755fd0bd08078bd66a283f7
Emmanuel <emmanuelugwueze6@gmail.com>
2022-06-22 21:02:19 +0100

    created new user service

--- services/user.service.js
+++ services/user.service.js
@@ -1,8 +1,6 @@
-import userModel from "../models/user.model.js";
+import userModel from '../models/user.model.js';
 
-
-const createUser = async(data) => {
-    const newUser = await userModel.create(data);
-    return newUser;
-
-}
+const createUser = async (data) => {
+  const newUser = await userModel.create(data);
+  return newUser;
+};
