e2eeeea1d8918c251bbc567b2add09045cc7e58b
Emmanuel <emmanuelugwueze6@gmail.com>
2022-06-23 10:48:39 +0100

    fixes

--- config/db.config.js
+++ config/db.config.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const database = () => {
-  mongoose.connect(process.env.DATABASE_URI).then((value) => console.log('database connected')).catch((err) => console.log(err));
+  mongoose.connect(process.env.MONGODB_URI).then((value) => console.log('database connected')).catch((err) => console.log(err));
 };
 
 export default database;
