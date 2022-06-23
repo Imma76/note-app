939817a4be9a7753b43cd56ccc0d7068053781e2
Emmanuel <emmanuelugwueze6@gmail.com>
2022-06-23 14:00:06 +0100

    route fix

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -1,16 +1,17 @@
 import express from 'express';
+import compression from 'compression';
+import cors from 'cors';
 import database from '../config/db.config.js';
 import errorHandler from './error.handler.js';
 import noteRouter from '../routes/notes.routes.js';
 import router from '../routes/index.routes.js';
-//import compression from 'compression';
-
 
 const middleware = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(router);
-  //app.use(compression);
+  app.use(compression);
+  app.use(cors());
   database();
   app.use(errorHandler);
 };
