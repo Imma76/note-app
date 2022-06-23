4116c332c8dd67907834169bdda72841b167760f
Emmanuel <emmanuelugwueze6@gmail.com>
2022-06-23 10:07:01 +0100

    fixes

--- middlewares/middleware.js
+++ middlewares/middleware.js
@@ -3,11 +3,14 @@ import database from '../config/db.config.js';
 import errorHandler from './error.handler.js';
 import noteRouter from '../routes/notes.routes.js';
 import router from '../routes/index.routes.js';
+import compression from 'compression';
+
 
 const middleware = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(router);
+  //app.use(compression);
   database();
   app.use(errorHandler);
 };
